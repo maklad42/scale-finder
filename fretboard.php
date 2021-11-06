a1e9c67a80ba60249fe5c864e2e328c7354e8a84
maklad42 <maklad42+git@gmail.com>
2021-11-06 18:30:53 +0900

    Fixes Issue 2: Adds Metal Lord font to currently selected scale name. 211107

--- fretboard.php
+++ fretboard.php
@@ -1,19 +1,19 @@
 <?php
 
-if(isset($_POST['scalekey'])) {
+if (isset($_POST['scalekey'])) {
   $key = $_POST['scalekey'];
 } else {
   $key = "C";
 }
 
-if(isset($_POST['scalemode'])) {
+if (isset($_POST['scalemode'])) {
   $mode = $_POST['scalemode'];
 } else {
   $mode = "Ionian (Major)";
 }
 
 
-echo "Showing: ".$key." ".$mode;
+echo "<p class='showing'><span class='show-name'>Showing:</span> " . $key . " " . $mode . "</p>";
 
 
 // Create switch for key
@@ -24,7 +24,7 @@ switch ($key) {
   case "C#/Db":
     $startkey = "11";
     break;
- case "D":
+  case "D":
     $startkey = "10";
     break;
   case "D#/Eb":
@@ -57,7 +57,7 @@ switch ($key) {
     break;
   default:
     $startkey = 99;
-    $rootnote ="";
+    $rootnote = "";
 }
 
 
@@ -84,8 +84,8 @@ switch ($mode) {
   case "Locrian":
     $startmode = "11";
     break;
-    default:
-      $startmode = 99;
+  default:
+    $startmode = 99;
 }
 
 // Set variable to count starting position in fretboard array
@@ -95,10 +95,10 @@ $startpos = $startkey + $startmode;
 $mark = array("", "", "", "o", "", "o", "", "o", "", "o", "", "", "8");
 
 //Scale sequence starting at E and representing the C Major (no sharps or flats) scale
-$fretboard = array(" on"," on first",""," on",""," on fifth",""," on"," on"," ninth"," on",""," on"," on first",""," on",""," on fifth",""," on"," on"," ninth"," on","", " on");
-$strings = array("E1","B","G","D","A","E2");  // Set names for each string to set as classes
+$fretboard = array(" on", " on first", "", " on", "", " on fifth", "", " on", " on", " ninth", " on", "", " on", " on first", "", " on", "", " on fifth", "", " on", " on", " ninth", " on", "", " on");
+$strings = array("E1", "B", "G", "D", "A", "E2");  // Set names for each string to set as classes
 $startfret = array(0, 7, 3, 10, 5, 0);  // Offset for each string to align starting position
-$rootnote =$startmode + 8;  // Set position of root note for the scale to add "red" class
+$rootnote = $startmode + 8;  // Set position of root note for the scale to add "red" class
 
 // Start table
 echo "<table class='fretboard'>";
@@ -106,40 +106,40 @@ echo "<table class='fretboard'>";
 // Draw in fret markings
 echo "<tr>";
 $fretcount = 0;
-for($fm = 1; $fm<=25; $fm++) {
-  if($fretcount > 12) {
+for ($fm = 1; $fm <= 25; $fm++) {
+  if ($fretcount > 12) {
     $fretcount = 1;
   }
-  echo "<td>".$mark[$fretcount]."</td>";
+  echo "<td>" . $mark[$fretcount] . "</td>";
   $fretcount++;
 }
 echo "</tr>";
 
 //Set loop to draw six strings
-for($i = 0; $i<=5; $i++) {
-  echo "<tr class='".$strings[$i]."-string'>";
+for ($i = 0; $i <= 5; $i++) {
+  echo "<tr class='" . $strings[$i] . "-string'>";
 
   //Set loop to draw 24 frets on each of the six strings
 
   // Initialise starting position counter
   $go = $startpos + $startfret[$i];
 
-  for($b = 0; $b <= 24; $b++) {
+  for ($b = 0; $b <= 24; $b++) {
 
-    if($go>23) {
+    if ($go > 23) {
       $go = $go - 24;
     }
 
     // Add variable to count position. Max 24, start position from key and mode switches
 
     // Add class from fretboard array
-    if($b == 0) {
+    if ($b == 0) {
       echo "<td class='nut";
-    }else{
+    } else {
       echo "<td class='fret";
     }
     echo $fretboard[$go];
-    if($go == $rootnote || $go == $rootnote +12 || $go == $rootnote -12) {
+    if ($go == $rootnote || $go == $rootnote + 12 || $go == $rootnote - 12) {
       echo " red";
     }
     echo "'></td>";
@@ -149,5 +149,3 @@ for($i = 0; $i<=5; $i++) {
 }
 
 echo "</table>";
-
-?>
